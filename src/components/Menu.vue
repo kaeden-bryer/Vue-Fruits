73b799fa439873badd86ccf51d075b470a124002
HououinKyouma555 <130073365+HououinKyouma555@users.noreply.github.com>
2026-01-07 22:53:07 -0500

    Create menu and menu item

---
+++ src/components/Menu.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import MenuItem from './MenuItem.vue'
+</script>
+
+<template>
+    <h1>Menu</h1>
+    <div class="menu">
+        <MenuItem image="/apple.avif" bgColor="red"/>
+        <MenuItem image="/banana.avif" bgColor="lightblue"/>
+        <MenuItem image="/blueberry.avif" bgColor="blue"/>
+        <MenuItem image="/coconut.avif" bgColor="brown"/>
+        <MenuItem image="/grapefruit.avif" bgColor="orange"/>
+        <MenuItem image="/lemon.avif" bgColor="yellow"/>
+        <MenuItem image="/papaya.avif" bgColor="orange"/>
+        <MenuItem image="/pineapple.avif" bgColor="yellow"/>
+        <MenuItem image="/pomegranate.avif" bgColor="red"/>
+        <MenuItem image="/raspberry.avif" bgColor="red"/>
+        <MenuItem image="/kiwi.avif" bgColor="green"/>
+        <MenuItem image="/watermelon.avif" bgColor="green"/>
+    </div>
+</template>
+
+<style scoped>
+
+.menu {
+  display: flex;
+  width: 80vw;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-self: center;
+  background: pink;
+  justify-content: center;
+  gap: 2em 2em;
+  padding: 2em;
+}
+
+</style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+defineProps<{
+    image: string,
+    bgColor: string
+}>()
+</script>
+
+<template>
+    <img :src="image" :style="{ '--color': bgColor }" class="menu-item" />
+</template>
+
+<style scoped>
+.menu-item {
+  height: 15em;
+  will-change: filter;
+  transition: filter 300ms;
+  border-radius: 20px;
+}
+.menu-item:hover {
+  filter: drop-shadow(0 0 2em var(--color));
+  transform: scale(1.05);
+  transition: transform 200ms ease-in-out;
+}
+</style>
